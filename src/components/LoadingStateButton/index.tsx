410729b114b69332f72e19a5d973b731e0f032f2
Beethov KJ <beethovkj@gmail.com>
2023-06-28 17:48:32 +0530

    Update button with more control to consumer

--- src/components/LoadingStateButton/index.tsx
+++ src/components/LoadingStateButton/index.tsx
@@ -3,16 +3,9 @@ import React from 'react';
 
 import { LoadingStateButtonProps } from './types';
 
-const LoadingStateButton = ({
-  label,
-  type = 'button',
-  size = 'large',
-  disabled = false,
-  isLoading = false,
-  ...props
-}: LoadingStateButtonProps) => {
+const LoadingStateButton = ({ label, isLoading = false, ...props }: LoadingStateButtonProps) => {
   return (
-    <Button aria-label={label} type={type} size={size} {...props}>
+    <Button aria-label={label} {...props}>
       {isLoading ? <div className="btn-loading">Loading ...</div> : label}
     </Button>
   );
